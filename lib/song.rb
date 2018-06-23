90dd62b4a1fab2238a60a99df7faa5850febd5c7
Leticia Molina <letimolina@gmail.com>
2018-06-23 17:29:06 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,19 @@
-class Song 
-  attr_accessor :name, :artists, :genre
-  
-  
+class Song
+  attr_accessor :name, :artist, :genre
+
   @@all = []
-  
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@all << self
   end
-    
-  def self.all 
+
+  def self.all
     @@all
   end
-  
+
 end
     
     
